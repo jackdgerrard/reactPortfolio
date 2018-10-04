771cdeb5a6ebb67ff54608eebab243061e67e6c9
jackdgerrard <jackdgerrard@gmail.com>
2018-10-04 15:23:11 +0100

    updated codepen link

--- src/components/social.js
+++ src/components/social.js
@@ -3,7 +3,9 @@
  ************/
 
 //react
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 
 /*************
  ** social networks
@@ -11,29 +13,46 @@ import React, { Component } from "react";
 
 class social extends Component {
   render() {
-    return (
-      <section id="social">
-        <h2>Social Networks And Other Platforms</h2>
+    return ( <
+      section id = "social" >
+      <
+      h2 > Social Networks And Other Platforms < /h2>
 
-        <a href="https://twitter.com/Jack_Grrd" id="twitter">
-          <i class="fa fa-twitter-square" aria-hidden="true" />
-        </a>
-        <a href="https://github.com/jackdgerrard" id="github">
-          <i className="fa fa-github" aria-hidden="true" />
-        </a>
-        <a href="https://www.instagram.com/jack_grrd/?hl=en" id="instagram">
-          <i class="fa fa-instagram" aria-hidden="true" />
-        </a>
-        <a href="https://codepen.io/" id="codepen">
-          <i className="fa fa-codepen" aria-hidden="true" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/jack-gerrard-627bb9107/"
-          id="linkedIn"
-        >
-          in
-        </a>
-      </section>
+      <
+      a href = "https://twitter.com/Jack_Grrd"
+      id = "twitter" >
+      <
+      i class = "fa fa-twitter-square"
+      aria - hidden = "true" / >
+      <
+      /a> <
+      a href = "https://github.com/jackdgerrard"
+      id = "github" >
+      <
+      i className = "fa fa-github"
+      aria - hidden = "true" / >
+      <
+      /a> <
+      a href = "https://www.instagram.com/jack_grrd/?hl=en"
+      id = "instagram" >
+      <
+      i class = "fa fa-instagram"
+      aria - hidden = "true" / >
+      <
+      /a> <
+      a href = "https://codepen.io/spectral"
+      id = "codepen" >
+      <
+      i className = "fa fa-codepen"
+      aria - hidden = "true" / >
+      <
+      /a> <
+      a href = "https://www.linkedin.com/in/jack-gerrard-627bb9107/"
+      id = "linkedIn" >
+      in
+      <
+      /a> <
+      /section>
     );
   }
 }
